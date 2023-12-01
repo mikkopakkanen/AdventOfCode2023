4bfb74edf602761bbea955ade9f5e45a5bb87b96
Mikko Pakkanen <mikko.pakkanen@easyopp.fi>
2023-12-02 01:01:16 +0200

    Day1 pt 2

--- 1/2/index.js
+++ 1/2/index.js
@@ -1,5 +1,19 @@
 import { readInput } from '../../utils/readInput.js'
 
+const letterObj = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9
+}
+
+const re = new RegExp(/(?=(one|two|three|four|five|six|seven|eight|nine))/g);
+
 const sumFirstAndLast = (val) => {
   const numbers = val.filter(v => parseInt(v));
   return numbers.length > 1 
@@ -7,10 +21,23 @@ const sumFirstAndLast = (val) => {
     : parseInt(numbers[0] + numbers[0]);
 }
 
+const replaceLetters = (source, matches) => {
+  for(const match of matches) {
+    source = source.replaceAll(re, letterObj[match]);
+  }
+  return source;
+}
+
+const lettersToDigits = (source) => {
+  const matches = Array.from(source.matchAll(re), x => x[1])
+  return matches ? replaceLetters(source, matches) : null
+}
+
 const Day1Pt2 = () => {
   const input = readInput(1);
   const rows = Array.from(input.split("\r\n"));
-  const result = rows.reduce((total, current) => total + sumFirstAndLast([...current]), 0);
+  const modifiedRows = rows.map(lettersToDigits).filter(row => row);
+  const result = modifiedRows.reduce((total, current) => total + sumFirstAndLast([...current]), 0);
 
   console.log("Day 1 part 2:")
   console.log(result);
