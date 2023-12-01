9d0ab48a549c070a969c899582965544f1033b23
Mikko Pakkanen <mikko.pakkanen@easyopp.fi>
2023-12-01 18:48:56 +0200

    Day1 pt 1

--- 1/index.js
+++ 1/index.js
@@ -1,8 +1,17 @@
 const getInput = require('../getInput.js')
 
+const sumFirstAndLast = (val) => {
+  const numbers = val.filter(v => Number.isInteger(parseInt(v)));
+  return numbers.length > 1 
+    ? parseInt(numbers[0] + numbers[numbers.length-1])
+    : parseInt(numbers[0] + numbers[0]);
+}
+
 const day1 = () => {
-  const data = getInput(1)
-  console.log(data);
+  const data = getInput(1);
+  const rows = Array.from(data.split("\r\n"));
+  const total = rows.reduce((total, current) => total + sumFirstAndLast([...current]), 0);
+  console.log(total);
 }
 
 module.exports = day1
