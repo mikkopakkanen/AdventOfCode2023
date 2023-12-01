61646a74b266241e1bad9a2bc064d8471bfb6925
Mikko Pakkanen <mikko.pakkanen@easyopp.fi>
2023-12-01 20:49:09 +0200

    Add npm script for Day1

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 import { Day1Pt1, Day1Pt2 } from "./1/index.js";
 
-const Day1 = (part) => part === 1 ? Day1Pt1() : Day1Pt2()
-
-const main = () => {
-  Day1(1);
-}
-
-main();
+export const Day1 = (part) => part === 1 ? Day1Pt1() : Day1Pt2()
