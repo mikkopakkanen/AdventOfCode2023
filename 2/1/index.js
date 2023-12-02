ab1ad4beb053aa349d73f2bdf603feb753debe8d
Mikko Pakkanen <mikko.pakkanen@easyopp.fi>
2023-12-02 17:26:42 +0200

    Day2.1

--- 2/1/index.js
+++ 2/1/index.js
@@ -1,13 +1,59 @@
 import { readInput } from '../../utils/readInput.js'
 
-const Day2Pt1 = () => {
-  const input = readInput(2, 1, true);
-  const rows = Array.from(input.split("\r\n"));
-  console.log(rows);
+const limits = {
+  red: 12,
+  green: 13,
+  blue: 14
+}
 
-  const result = "foobar"
+const splitCubes = (set) => {
+  const cubes = set.toString().replaceAll(" ", "").split(",")
+  let results = []
+  for(const cube of cubes) {
+    const matches = cube.match(/\d+|blue|red|green|^/g)
+    results.push({ color: matches[1], amount: matches[0] })
+  }
+  return results
+}
+
+const splitSets = (row) => {
+  const sets = row.split(";")
+  return splitCubes(sets)
+}
+
+const splitGames = (rows) => {
+  const games = []
+  for (const row of rows) {
+    const splitted = row.split(/Game \d+: /g)
+    games.push(splitSets(splitted[1]))
+  }
+  return games
+}
+
+const getPossibleGames = (games) => {
+  const possibleGames = games.map((set) => {
+    return set.map(color => color.amount <= limits[color.color])
+  })
+  return possibleGames.map(g => !g.some(v => v === false))
+}
+
+const sumIndices = (possibleGames) => {
+  return possibleGames.reduce((agg, possible, index) => {
+    index++
+    if(possible) agg = agg + index
+    return agg
+  }, 0)
+}
+
+const Day2Pt1 = () => {
+  const input = readInput(2)
+  const rows = Array.from(input.split("\r\n"))
+
+  const possibleGames = getPossibleGames(splitGames(rows))
+  const result = sumIndices(possibleGames)
+
   console.log("Day 2 part 1:")
-  console.log(result);
+  console.log(result)
 }
 
 export default Day2Pt1
