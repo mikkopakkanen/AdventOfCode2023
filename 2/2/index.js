5f21ff35c311355dfd70a6b765bf2a4f06d889c4
Mikko Pakkanen <mikko.pakkanen@easyopp.fi>
2023-12-02 18:02:55 +0200

    Day 2.2

--- 2/2/index.js
+++ 2/2/index.js
@@ -1,11 +1,5 @@
 import { readInput } from '../../utils/readInput.js'
 
-const limits = {
-  red: 12,
-  green: 13,
-  blue: 14
-}
-
 const splitCubes = (set) => {
   const cubes = set.toString().replaceAll(" ", "").split(",")
   let results = []
@@ -30,27 +24,39 @@ const splitGames = (rows) => {
   return games
 }
 
-const getPossibleGames = (games) => {
-  const possibleGames = games.map((set) => {
-    return set.map(color => color.amount <= limits[color.color])
-  })
-  return possibleGames.map(g => !g.some(v => v === false))
-}
-
-const sumIndices = (possibleGames) => {
-  return possibleGames.reduce((agg, possible, index) => {
-    index++
-    if(possible) agg = agg + index
+const sumIndices = (values) => {
+  const tmp = values.reduce((agg, game) => {
+    const multiply = game.reduce((acc, val) => acc = acc * parseInt(val.amount), 1)
+    agg += multiply
     return agg
   }, 0)
+  return tmp
+}
+
+const groupByLargestValue = (games) => {
+  const summedAmounts = []
+  for(const game of games) {
+    const amounts = Object.values(
+      game.reduce((agg, game) => {
+        if (agg[game.color] === undefined) agg[game.color] = { color: game.color, amount: 0 }
+        agg[game.color].amount = +game.amount > +agg[game.color].amount 
+          ? game.amount
+          : agg[game.color].amount
+        return agg
+      }, {})
+    )
+    summedAmounts.push(amounts)
+  }
+  return summedAmounts
 }
 
 const Day2Pt2 = () => {
-  const input = readInput(2, 2, true)
+  const input = readInput(2, 2)
   const rows = Array.from(input.split("\r\n"))
 
-  const possibleGames = getPossibleGames(splitGames(rows))
-  const result = sumIndices(possibleGames)
+  const splitted = splitGames(rows);
+  const values = groupByLargestValue(splitted)
+  const result = sumIndices(values)
 
   console.log("Day 2 part 1:")
   console.log(result)
