f4121cbd1d162adec5d77d62f877c94f258f024b
Mikko Pakkanen <mikko.pakkanen@easyopp.fi>
2023-12-02 17:35:46 +0200

    Day 2.2 setup

--- 2/index.js
+++ 2/index.js
@@ -1,3 +1,4 @@
 import Day2Pt1 from "./1/index.js";
+import Day2Pt2 from "./2/index.js";
 
-export { Day2Pt1 }
+export { Day2Pt1, Day2Pt2 }
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import { Day1Pt1, Day1Pt2 } from "./1/index.js";
-import { Day2Pt1 } from "./2/index.js";
+import { Day2Pt1, Day2Pt2 } from "./2/index.js";
 
 export { 
   Day1Pt1,
   Day1Pt2,
-  Day2Pt1
+  Day2Pt1,
+  Day2Pt2
 }
