e69c27eb6b61d1e8bca082d3362b7506b8ffb033
Mikko Pakkanen <mikko.pakkanen@easyopp.fi>
2023-12-04 21:15:20 +0200

    Day 4.1

--- 4/1/index.js
+++ 4/1/index.js
@@ -1,9 +1,37 @@
 import { readInput } from '../../utils/readInput.js'
 
+const splitNumbers = (hands) => {
+  const winningNumbers = hands[0].split(" ").filter(v => v)
+  const playerNumbers = hands[1].split(" ").filter(v => v)
+  return playerNumbers.map(p => winningNumbers.includes(p) ? p : null).filter(v => v)
+}
+
+const splitHands = (row) => {
+  const hands = row.split("|")
+  return splitNumbers(hands)
+}
+
+const splitGames = (rows) => {
+  const games = []
+  for (const row of rows) {
+    const splitted = row.split(/Card\s+\d+: /g)
+    const hands = splitHands(splitted[1]);
+    if(hands.length > 0 ) games.push(hands)
+  }
+  return games
+}
+
 const Day4Pt1 = () => {
-  const input = readInput(4, 1, true)
+  const input = readInput(4)
   const rows = Array.from(input.split("\r\n"))
-  const result = "foobar"
+  const matches = splitGames(rows);
+  const result = matches.map(m => m.reduce((acc, val, i) => {
+    if(m.length === 1) return 1
+    if(i === 1) acc = 1
+    acc = acc * 2
+    return acc
+  }, 1)).reduce((acc, val) => acc + val, 0);
+
   console.log("Day 4 part 1:")
   console.log(result)
 }
